ec2d6bcaaa637ac23df63b97ac14aac2e15fe239
ZhuoranWang <41529680+wang-zhuoran@users.noreply.github.com>
2021-06-27 11:21:36 +0800

    Update test.py

--- Testing_model/test.py
+++ Testing_model/test.py
@@ -14,7 +14,7 @@ while 1:
     for x, y, w, h in faces:
       roi_gray = gray[y:y + h, x:x + h]
       roi_color = frame[y:y + h, x:x + h]
-      cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+      # cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
       faces = faceCascade.detectMultiScale(roi_gray)
       for (ex, ey, ew, eh) in faces:
           face_roi = roi_color[ey:ey + eh, ex:ex + eh]
@@ -27,6 +27,13 @@ while 1:
 
           Predictions = new_model.predict(final_image)
           print(Predictions)
+          if (Predictions > 0.5):
+              cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+              cv2.putText(frame, 'Wearing Mask!', (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 1)
+              # cv2.putText(img, str,origin,font,size,color,thickness)
+          else:
+              cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+              cv2.putText(frame, 'No Mask!', (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 1)
 
           # if(Predictions<0.45):
           #   print("No mask")
@@ -36,6 +43,7 @@ while 1:
           #   print("Can not determine")
 
       cv2.imshow("capture", frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
